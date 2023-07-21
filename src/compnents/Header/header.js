581c89823108879eceb84a63accae4d80d5abf18
Subhash <subhashkjaiswal333@gmail.com>
2023-07-21 11:02:04 +0530

    header added

---
+++ src/compnents/Header/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+    <header className="App-header">
+        <h1>Subhash Kumar Jaiswal</h1>
+        <h4>Web Developer</h4>
+    </header>)
+}
