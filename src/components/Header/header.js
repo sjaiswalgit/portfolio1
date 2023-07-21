cd3409ec0b1828678816b7153fcbdfb098f67555
Subhash <subhashkjaiswal333@gmail.com>
2023-07-21 20:19:08 +0530

    social links fixed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,6 +4,6 @@ export default function Header(){
     return(
     <header className="App-header">
         <h1>Subhash Kumar Jaiswal</h1>
-        <h4>Web Developer</h4>
+        <h4><span className='green_dot'></span>Web Developer</h4>
     </header>)
 }
--- src/components/Social/social.js
+++ src/components/Social/social.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Socials(){
     return(
     <div>
-        <a href="https://www.facebook.com/subhash.jaiswal.9699"><i className="fa-brands fa-facebook-f"></i></a>
-        <a href="https://www.linkedin.com/in/subhash-kumar-jaiswal-2199951a8"><i className="fa-brands fa-linkedin-in"></i></a>
-        <a href="https://www.instagram.com/subhash.jaiswal.9699"><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://www.facebook.com/subhash.jaiswal.9699" target="_blank"  rel="noreferrer"><i className="fa-brands fa-facebook-f"></i></a>
+        <a href="https://www.linkedin.com/in/subhash-kumar-jaiswal-2199951a8" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://www.instagram.com/subhash.jaiswal.9699" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
     </div>
     )
 }
