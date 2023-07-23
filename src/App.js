58f574ff05d929c76d714551c68ae0685c41c6d3
Subhash <subhashkjaiswal333@gmail.com>
2023-07-23 16:58:57 +0530

    Pages and Routing added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import './App.css'
 import React from 'react'
-import Intro from './components/Intro/intro'
 import Contact from './components/Contact/contact'
 import Footer from './components/Footer/footer'
 import Header from './components/Header/header'
-import Card from './components/Card/card'
-import NavBar from './components/Navbar/navbar';
+import About from './pages/About/about'
+import ContactForm from './pages/ContactForm/contactform'
+import Resume from './pages/Resume/resume'
+import Project from './pages/Project/project'
+import {Routes as Switch,Route} from 'react-router-dom'
 import {connect} from "react-redux";
 import {toggle} from './redux/index'
 const App = (props) => {
-  return (
-    <div className={`App ${props.navBar?'stop_overflow':''}`}>
-      <NavBar/>
-      <div className='about'>
-        <Header />
-        <Card/>
-        <Intro/>
-      </div>
-      <div className="contact">
+  const [app,setApp]=React.useState('')
+  function show(){
+    if(document.documentElement.scrollHeight-window.innerHeight===Math.ceil(window.scrollY)){
+      setApp('show')
+    }
+    // else{
+    //   if(app==='show'){
+    //     setApp('');
+    //   }
+    // }
+  }
+  React.useEffect(()=>{
+  window.addEventListener('scroll',show)},[])
+  return (<div className={`App ${props.navBar?'stop_overflow':''}`}>
+    <Header/>
+    <Switch>
+        <Route exact path="/" element={<About/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/project' element={<Project/>}/>
+        <Route path='/contact' element={<ContactForm/>}/>
+    </Switch>
+    <div className='contact'>
         <Contact/>
         <Footer/>
       </div>
+      <button className={`backToTop ${app}`} onClick={()=>window.scrollTo(0,0)}> <i className="fa-solid fa-arrow-up"></i></button>
     </div>
   )
 }
---
+++ src/pages/ContactForm/contactform.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div>C</div>
+  )
+}
+
+export default ContactForm
